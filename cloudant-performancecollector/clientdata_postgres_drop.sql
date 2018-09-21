4f725f985a3538c037596fff3a53253ede0dd991
Andrew Rombach <andrew.rombach@uk.ibm.com>
2018-09-21 09:39:50 +0100

    Schema for client oriented data collection

---
+++ cloudant-performancecollector/clientdata_postgres_drop.sql
@@ -0,0 +1,19 @@
+drop if exists index client_verb_stats_idx1 ;
+drop if exists index client_verb_stats_idx2 ;
+drop if exists index client_verb_stats_idx3 ;
+drop if exists index client_verb_stats_idx4 ;
+
+drop if exists index client_endpoint_stats_idx1 ;
+drop if exists index client_endpoint_stats_idx2 ;
+
+drop if exists index client_document_stats_idx1 ;
+drop if exists index client_document_stats_idx2 ;
+
+drop if exists index client_all_stats_idx1;
+
+drop if exists table client_document_stats ;
+drop if exists table client_endpoint_stats ;
+drop if exists table client_verb_stats ;
+drop if exists table client_stats ;
+drop if exists table client_all_stats ;
+
