ea69d0d783015b630a16b12def45e97331bce7ee
c02vm1 <c02vm1@c02vm1s-mbp.bristol.uk.ibm.com>
2018-11-13 15:12:56 +0000

    Minor changes to sql script

--- cloudant-performancecollector/bodydata_postgres.sql
+++ cloudant-performancecollector/bodydata_postgres.sql
@@ -2,9 +2,6 @@
 create table body_endpoint_stats (index int ,cluster varchar(255), loghost varchar(255), client varchar(255) ,database varchar(1024), verb varchar(25) , endpoint varchar(1024), body varchar(2048),mtime bigint ,mtime_epoch int, tqmin int ,tqavg decimal,tqmax int,tqcount int,tqsum int,trmin int,travg decimal,trmax int,trcount int,trsum bigint,ttmin int ,ttavg decimal ,ttmax int,ttcount int,ttsum bigint,ttrmin int,ttravg decimal,ttrmax int,ttrcount int,ttrsum bigint,szmin int,szavg decimal,szmax int,szcount int,szsum bigint,st2count int,st3count int,st4count int,st5count int,stfailpct int);
 
 
-
-
-
 create index body_endpoint_stats_idx1 on body_endpoint_stats (client,mtime_epoch);
 create index body_endpoint_stats_idx2 on body_endpoint_stats (client,mtime_epoch,database,verb,endpoint);
 create index body_endpoint_stats_idx3 on body_endpoint_stats (cluster,client,mtime_epoch,database,verb,endpoint,body);
