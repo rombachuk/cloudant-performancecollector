5f796b2b2efe2c16945098759fe892f4216c8cc4
c02vm1 <c02vm1@c02vm1s-mbp.hursley.uk.ibm.com>
2019-01-29 14:26:13 +0000

    Improve patchscript to use crontab_example

--- deploy/patch_install.sh
+++ deploy/patch_install.sh
@@ -6,6 +6,7 @@ mkdir /opt/cloudant-performancecollector-bkp-$now/init.d
 cp /etc/init.d/cpc_api_processor /opt/cloudant-performancecollector-bkp-$now/init.d
 cp -r ../cloudant-performancecollector/*.py /opt/cloudant-performancecollector
 cp -r ../cloudant-performancecollector/perfagent_cronscript/*.sh /opt/cloudant-performancecollector/perfagent_cronscript
+cp -r ../cloudant-performancecollector/perfagent_cronscript/crontab_example /opt/cloudant-performancecollector/perfagent_cronscript
 cp /opt/cloudant-performancecollector/cpc_api_processor /etc/init.d
 systemctl enable cpc_api_processor
 systemctl start cpc_api_processor
