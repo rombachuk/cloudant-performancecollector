254f48ce066dbc26dd10dbc4d4ad6e13ad7f3054
Andrew-Rombach <andrew.rombach@uk.ibm.com>
2019-07-17 09:37:47 +0100

    Correction to scripts for directory changes

--- cloudant-performancecollector/resources/collect/scripts/bodydata_minute_collect.sh
+++ cloudant-performancecollector/resources/collect/scripts/bodydata_minute_collect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # set -x
 stats=`echo "bodystats_body_by_minute_"$1"_to_"$2`
-conninfo=`echo "/opt/cloudant-performancecollector/perfagent_connection.info"`
+conninfo=`echo "/opt/cloudant-performancecollector/resources/collect/configuration/perfagent_connection.info"`
 /usr/bin/python /opt/cloudant-performancecollector/bodydata_collect.py -O csv -s body -g minute -f $1 -t $2 -L $3 -H $HOSTNAME -x $conninfo
--- cloudant-performancecollector/resources/collect/scripts/clientdata_minute_collect.sh
+++ cloudant-performancecollector/resources/collect/scripts/clientdata_minute_collect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # set -x
 stats=`echo "clientstats_"$1"_by_minute_"$2"_to_"$3`
-conninfo=`echo "/opt/cloudant-performancecollector/perfagent_connection.info"`
+conninfo=`echo "/opt/cloudant-performancecollector/resources/collect/configuration/perfagent_connection.info"`
 /usr/bin/python /opt/cloudant-performancecollector/clientdata_collect.py -O csv -s $1 -g minute -f $2 -t $3 -L $4 -H $HOSTNAME -x $conninfo
