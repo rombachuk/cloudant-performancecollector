a6cd506ef312af82d1acd20e969a07114a698144
Andrew-Rombach <andrew.rombach@uk.ibm.com>
2019-07-17 11:36:44 +0100

    Add zip to continuous haproxy files

--- cloudant-performancecollector/resources/export/postgres/scripts/continuous/proxydata_every_minute.sh
+++ cloudant-performancecollector/resources/export/postgres/scripts/continuous/proxydata_every_minute.sh
@@ -6,7 +6,7 @@ let toepoch=$nowepoch-$3*60
 frompretty=`date -d @$fromepoch +%Y%m%d%H%M`
 topretty=`date -d @$toepoch +%Y%m%d%H%M`
 fromgrep=`date -d @$fromepoch +%d/%b/%Y:%H:%M`
-logfile=`echo "/opt/cloudant-performancecollector/tmp/"proxy$HOSTNAME"_"$1"_"$frompretty"_haproxy.log"`
-tail -n 500000 /var/log/haproxy.log | grep $fromgrep > $logfile
+logfile=`echo "/opt/cloudant-performancecollector/tmp/"proxy$HOSTNAME"_"$1"_"$frompretty"_haproxy.log.gz"`
+tail -n 500000 /var/log/haproxy.log | grep $fromgrep | gzip > $logfile
 /opt/cloudant-performancecollector/resources/collect/scripts/proxydata_minute_collect.sh $1 $frompretty $topretty $logfile
 /opt/cloudant-performancecollector/resources/export/postgres/scripts/onetime/proxydata_minute_load.sh $1 $frompretty $topretty
