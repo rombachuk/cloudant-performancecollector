d81a4b043164b817b4654ffd7f1149a3ee892857
Andrew-Rombach <andrew.rombach@uk.ibm.com>
2019-07-17 09:50:38 +0100

    Fixes to deploy scripts to optionalise postgres

--- deploy/clean_install.sh
+++ deploy/clean_install.sh
@@ -7,42 +7,38 @@ cp /etc/init.d/cpc_api_processor /opt/cloudant-performancecollector-bkp-$now/ini
 rm -rf /opt/cloudant-performancecollector
 rm -f /etc/init.d/cpc_api_processor
 cp -r ../cloudant-performancecollector /opt
-cp /opt/cloudant-performancecollector/cpc_api_processor /etc/init.d
+cp /opt/cloudant-performancecollector/resources/collect/scripts/cpc_api_processor /etc/init.d
 systemctl enable cpc_api_processor
 systemctl start cpc_api_processor
-read -p 'Drop and recreate postgres schema (y/n): ' confirm
-if [ "$confirm" == "y" ]
+read -p 'Export to Postgres (y/n): ' confirmpg
+if [ "$confirmpg" == "y" ]
 then
- if [[ -z $1 ]]
- then
-  read -p 'Postgres Host: ' host
- else
- host=$1
- fi
- if [[ -z $2 ]]
- then
-  read -sp 'Postgres Cloudant password: ' cpwd
- else
-  cpwd=$2
- fi
- export PGPASSWORD=$cpwd
+read -p 'Drop and recreate postgres schema (y/n): ' confirmpgschema
+if [ "$confirmpgschema" == "y" ]
+then
+ pghost=`cat /opt/cloudant-performancecollector/resources/export/postgres/configuration/perfagent_pg_db.info | cut -d ":" -f 1`
+ pgdb=`cat /opt/cloudant-performancecollector/resources/export/postgres/configuration/perfagent_pg_db.info | cut -d ":" -f 2`
+ pguser=`base64 --decode /opt/cloudant-performancecollector/resources/export/postgres/configuration/perfagent_pg_credentials.info | cut -d ":" -f 1`
+ PGPASSWORD=`base64 --decode /opt/cloudant-performancecollector/resources/export/postgres/configuration/perfagent_pg_credentials.info | cut -d ":" -f 2`
+ export PGPASSWORD
  echo 'dropping existing postgres proxydata schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/proxydata_postgres_drop.sql -o proxyschema_drop.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/proxydata_postgres_drop.sql -o proxyschema_drop.log
  echo 'creating new postgres proxydata schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/proxydata_postgres.sql -o proxyschema_create.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/proxydata_postgres.sql -o proxyschema_create.log
  echo 'dropping existing postgres metricsdb-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/metricsdbdata_postgres_drop.sql -o metricsdbschema_drop.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/metricsdbdata_postgres_drop.sql -o metricsdbschema_drop.log
  echo 'creating new postgres metricsdb-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/metricsdbdata_postgres.sql -o metricsdbschema_create.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/metricsdbdata_postgres.sql -o metricsdbschema_create.log
   echo 'dropping existing postgres volume-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/volumedata_postgres_drop.sql -o volumeschema_drop.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/volumedata_postgres_drop.sql -o volumeschema_drop.log
  echo 'creating new postgres volume-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/volumedata_postgres.sql -o volumeschema_create.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/volumedata_postgres.sql -o volumeschema_create.log
   echo 'dropping existing postgres volume-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/clientdata_postgres_drop.sql -o clientschema_drop.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/clientdata_postgres_drop.sql -o clientschema_drop.log
  echo 'creating new postgres volume-data schema'
- /usr/bin/psql -U cloudant -d postgres -h $host -f ../cloudant-performancecollector/clientdata_postgres.sql -o clientschema_create.log
+ /usr/bin/psql -U pguser -d $pgdb -h $pghost -f ../cloudant-performancecollector/resources/export/postgres/schema/clientdata_postgres.sql -o clientschema_create.log
 else
  echo 'no postgres schema changes'
+fi
 fi
 echo 'completed'
