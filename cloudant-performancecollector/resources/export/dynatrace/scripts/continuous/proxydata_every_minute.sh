fd532d2a0e548d34f4be70d4c2bf2c5fa8551927
Andrew Rombach <andrew.rombach@uk.ibm.com>
2021-10-07 15:47:54 +0100

    Additional changes after live testing

--- cloudant-performancecollector/resources/export/dynatrace/scripts/continuous/proxydata_every_minute.sh
+++ cloudant-performancecollector/resources/export/dynatrace/scripts/continuous/proxydata_every_minute.sh
@@ -7,10 +7,14 @@ today=`date -d @$fromepoch +%Y%m%d`
 frompretty=`date -d @$fromepoch +%Y%m%d%H%M`
 topretty=`date -d @$toepoch +%Y%m%d%H%M`
 fromgrep=`date -d @$fromepoch +%d/%b/%Y:%H:%M`
-logfile=`echo "/opt/cloudant-performancecollector/tmp/"proxy$HOSTNAME"_"$1"_"$frompretty"_haproxy.log.gz"`
-tail -n 500000 /var/log/haproxy.log | grep $fromgrep | gzip > $logfile
+logfile=`echo "/opt/cloudant-performancecollector/tmp/"finaltail$HOSTNAME"_"$frompretty"_haproxy.log.gz"`
+counter=0
+until [[ -f $logfile ]] || [[ $counter -gt 60 ]]
+ do
+ sleep 1
+ ((counter++))
+ done
 /opt/cloudant-performancecollector/resources/collect/scripts/proxydata_minute_collect.sh $1 $frompretty $topretty $logfile
-rm -f $logfile
 stats=`echo "stats_"$1"_by_minute_"$frompretty"_to_"$topretty`
 findfile="find /opt/cloudant-performancecollector/results -name "$stats"* | tail -n 1"
 statsfile=`eval $findfile`
---
+++ cloudant-performancecollector/resources/export/dynatrace/scripts/continuous/proxytail_every_minute.sh
@@ -0,0 +1,17 @@
+##!/bin/bash 
+# set -x
+nowepoch=`date +%s`
+let oldepoch=$nowepoch-$2*60-120
+let fromepoch=$nowepoch-$2*60
+let toepoch=$nowepoch-$3*60
+today=`date -d @$fromepoch +%Y%m%d`
+oldpretty=`date -d @$oldepoch +%Y%m%d%H%M`
+frompretty=`date -d @$fromepoch +%Y%m%d%H%M`
+topretty=`date -d @$toepoch +%Y%m%d%H%M`
+fromgrep=`date -d @$fromepoch +%d/%b/%Y:%H:%M`
+oldlogfile=`echo "/opt/cloudant-performancecollector/tmp/"finaltail$HOSTNAME"_"$oldpretty"_haproxy.log.gz"`
+tmplogfile=`echo "/opt/cloudant-performancecollector/tmp/"tail$HOSTNAME"_"$frompretty"_haproxy.log.gz"`
+finallogfile=`echo "/opt/cloudant-performancecollector/tmp/"finaltail$HOSTNAME"_"$frompretty"_haproxy.log.gz"`
+rm -f $oldlogfile
+tail -n 500000 /var/log/haproxy.log | grep $fromgrep | gzip > $tmplogfile
+mv $tmplogfile $finallogfile
