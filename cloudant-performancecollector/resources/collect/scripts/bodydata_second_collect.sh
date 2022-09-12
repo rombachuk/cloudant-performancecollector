ed03c3005bc287e3fbb0b6409f05224f2469c9d8
Andrew Rombach <andrew.rombach@uk.ibm.com>
2022-09-12 12:18:15 +0100

    Remove elastic search export and convert to python3

--- cloudant-performancecollector/resources/collect/scripts/bodydata_second_collect.sh
+++ cloudant-performancecollector/resources/collect/scripts/bodydata_second_collect.sh
@@ -3,4 +3,4 @@
 source /opt/cloudant-performancecollector/venv/bin/activate
 stats=`echo "bodystats_body_by_second_"$1"_to_"$2`
 conninfo=`echo "/opt/cloudant-performancecollector/resources/collect/configuration/perfagent_connection.info"`
-/opt/cloudant-performancecollector/venv/bin/python /opt/cloudant-performancecollector/bodydata_collect.py -O csv -s body -g second -f $1 -t $2 -L $3 -H $HOSTNAME -x $conninfo
+/opt/cloudant-performancecollector/venv/bin/python3 /opt/cloudant-performancecollector/bodydata_collect.py -O csv -s body -g second -f $1 -t $2 -L $3 -H $HOSTNAME -x $conninfo
--- cloudant-performancecollector/resources/collect/scripts/metricsdbdata_collect.sh
+++ cloudant-performancecollector/resources/collect/scripts/metricsdbdata_collect.sh
@@ -3,4 +3,4 @@
 now=`date +%s`
 source /opt/cloudant-performancecollector/venv/bin/activate
 conninfo=`echo "/opt/cloudant-performancecollector/resources/collect/configuration/perfagent_connection.info"`
-/opt/cloudant-performancecollector/venv/bin/python /opt/cloudant-performancecollector/metricsdb_collect.py -f $1 -x $conninfo
+/opt/cloudant-performancecollector/venv/bin/python3 /opt/cloudant-performancecollector/metricsdb_collect.py -f $1 -x $conninfo
