4320bac987194d100f8009f3c1c191657a5b7342
Andrew Rombach <andrew.rombach@uk.ibm.com>
2022-09-12 12:32:52 +0100

    Upgrade pip within venv to allow pandas to install

--- deploy/clean_install.sh
+++ deploy/clean_install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 now=`date +%Y%m%d%H%M` 
-systemctl stop cpc_api_processor
 cp -r /opt/cloudant-performancecollector /opt/cloudant-performancecollector-bkp-$now
 mkdir /opt/cloudant-performancecollector-bkp-$now/init.d
-cp /etc/init.d/cpc_api_processor /opt/cloudant-performancecollector-bkp-$now/init.d
 rm -rf /opt/cloudant-performancecollector
-rm -f /etc/init.d/cpc_api_processor
 cp -r ../cloudant-performancecollector /opt
 python3 -m venv /opt/cloudant-performancecollector/venv
 source /opt/cloudant-performancecollector/venv/bin/activate
+pip install --upgrade pip
 pip3 install requests
 pip3 install cython
 pip3 install numpy
